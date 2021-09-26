c886f4a57814a99af96a88c97540b348bca85a3a
DamageeZ <zhouzhm1@outlook.com>
2021-09-26 18:07:25 +0800

    主页面

--- main.cpp
+++ main.cpp
@@ -4,9 +4,22 @@
 using namespace std;
 #include "User.h"
 
+void Menu()
+{
+    cout << "***********************" << endl;
+    cout << "*****  1、今日打卡  *****" << endl;
+    cout << "*****  2、用户注册  *****" << endl;
+    cout << "*****  3、背诵单词  *****" << endl;
+    cout << "*****  4、词库管理  *****" << endl;
+    cout << "*****  5、导入词库  *****" << endl;
+    cout << "*****  6、退出  *****" << endl;
+    cout << "***********************" << endl;
+}
+
 int main()
 {
     std::cout << "Welcome to BDC\n";
+    Menu();
     const char* test = "abcd";
     User * u = new User();
     u->setUserName(test);
