16a158e9de1f119077113d89b2fdb66ea549dcd2
TTXBEB <hclegg374@gmail.com>
2021-09-27 11:21:41 +0800

    word

--- Word.cpp
+++ Word.cpp
@@ -1,5 +1,17 @@
-//
-// Created by zhouzhm1 on 2021/9/26.
-//
+/**
+ Created by zhouzhm1 on 2021/9/26.
+ Update by HCl on 2021/9/27.
+*/
 
+#include "stdio.h"
+#include "string.h"
+#include "stdlib.h"
 #include "Word.h"
+
+int word()
+{
+
+}
+
+
+
